076ae0d342a3e4b36986ce6e1805e96b4647d284
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2022-06-27 13:12:53 +0300

    exceptions.ts added

---
+++ src/common/enums/exceptions.ts
@@ -0,0 +1,8 @@
+export enum EXCEPTIONS {
+    INTERNAL_SERVER_ERROR = 'Internal Server Error',
+    USER_NOT_FOUND = 'User not found',
+    USER_ALREADY_EXISTS = 'User already exists',
+    USER_NOT_AUTHORIZED = 'User not authorized',
+    TASK_NOT_FOUND = 'Task not found',
+    INVALID_CREDENTIALS = 'Invalid credentials',
+}
---
+++ src/common/utils/errors.ts
@@ -0,0 +1,32 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { EXCEPTIONS } from '../enums/exceptions';
+
+export const InternalServerError = new HttpException(
+    EXCEPTIONS.INTERNAL_SERVER_ERROR,
+    HttpStatus.INTERNAL_SERVER_ERROR,
+);
+
+export const UnAuthorizedUser = new HttpException(
+    EXCEPTIONS.USER_NOT_AUTHORIZED,
+    HttpStatus.UNAUTHORIZED,
+);
+
+export const UserNotFound = new HttpException(
+    EXCEPTIONS.USER_NOT_FOUND,
+    HttpStatus.NOT_FOUND,
+);
+
+export const UserAlreadyExists = new HttpException(
+    EXCEPTIONS.USER_ALREADY_EXISTS,
+    HttpStatus.CONFLICT,
+);
+
+export const TaskNotFound = new HttpException(
+    EXCEPTIONS.TASK_NOT_FOUND,
+    HttpStatus.NOT_FOUND,
+);
+
+export const InvalidCredentials = new HttpException(
+    EXCEPTIONS.INVALID_CREDENTIALS,
+    HttpStatus.UNAUTHORIZED,
+);
